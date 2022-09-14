369dc3a4ccf10a387634841385bc9608ac0ff5c9
MohamedXx <mohamedsabry833331@gmail.com>
2022-09-14 02:24:11 +0200

    init project

---
+++ App.js
@@ -0,0 +1,15 @@
+import {View, Text} from 'react-native';
+import React, {useEffect} from 'react';
+import SplashScreen from 'react-native-splash-screen';
+
+export default function App() {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  );
+}
